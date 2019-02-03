4499fdd8ff990744e75affe6888a25ce2240efc7
Matheus Compri <matheus.compri@gmail.com>
2019-02-03 08:50:20 -0200

    fix: Updating Github Pages

---
+++ docs/build/precache-manifest.445886f8d1d05a2a76418c2b00459558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91bae0c5a90e1af9b739",
+    "url": "/productivity-checker/static/css/main.70ee9b69.chunk.css"
+  },
+  {
+    "revision": "91bae0c5a90e1af9b739",
+    "url": "/productivity-checker/static/js/main.91bae0c5.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/productivity-checker/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "17ad65a627a428e029ce",
+    "url": "/productivity-checker/static/js/runtime~main.17ad65a6.js"
+  },
+  {
+    "revision": "2069480242a0169c43dab5a5b9aa208b",
+    "url": "/productivity-checker/index.html"
+  }
+];
