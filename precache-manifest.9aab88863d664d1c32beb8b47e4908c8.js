267fc1b3922062c6bf7d5137611a50daf412e6a8
Matheus Compri <matheus.compri@gmail.com>
2019-02-10 10:57:23 -0200

    Updates

---
+++ precache-manifest.9aab88863d664d1c32beb8b47e4908c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d82a2fb74c4fc5dab642",
+    "url": "/productivity-checker/static/css/main.0dee84ee.chunk.css"
+  },
+  {
+    "revision": "d82a2fb74c4fc5dab642",
+    "url": "/productivity-checker/static/js/main.d82a2fb7.chunk.js"
+  },
+  {
+    "revision": "4e1e73da58eff4509d02",
+    "url": "/productivity-checker/static/js/1.4e1e73da.chunk.js"
+  },
+  {
+    "revision": "17ad65a627a428e029ce",
+    "url": "/productivity-checker/static/js/runtime~main.17ad65a6.js"
+  },
+  {
+    "revision": "810a0f25fba69956c168b3899bd2eea2",
+    "url": "/productivity-checker/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/productivity-checker/precache-manifest.6925bca6bd11c349c9c146bd961a15b1.js"
+  "/productivity-checker/precache-manifest.9aab88863d664d1c32beb8b47e4908c8.js"
 );
 
 workbox.clientsClaim();
